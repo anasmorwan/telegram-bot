c8e4a8d30594c21d20e501a4dec30ef80d28b14a
anasmorwan <anasmorwan5@gmail.com>
2026-04-17 15:50:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from schedule import scheduler
+from contents import send_messages, send_daily_post
+
+# الجمعة
+scheduler.add_job(send_messages, 'cron', day_of_week='fri', hour=9, minute=0)
+scheduler.add_job(send_messages, 'cron', day_of_week='fri', hour=16, minute=30)
+scheduler.add_job(send_messages, 'cron', day_of_week='fri', hour=20, minute=0)
+
+# كل يومين
+scheduler.add_job(send_daily_post, 'interval', days=2, hour=10, minute=0)
+
+scheduler.start()
 
