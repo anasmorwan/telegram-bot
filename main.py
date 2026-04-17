eaaf0ee303637eda57cb067965383316ca93478d
anasmorwan <anasmorwan5@gmail.com>
2026-04-17 15:53:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,33 @@ scheduler.add_job(send_daily_post, 'interval', days=2, hour=10, minute=0)
 
 scheduler.start()
 
+
+
+  # تعيين webhook قبل بدء الخادم
+  set_webhook()
+    
+  # تشغيل Flask في thread منفصل
+  t1 = Thread(target=run_flask)
+  t1.daemon = True
+  t1.start()
+    
+  # تشغيل البث في thread منفصل
+  t2 = Thread(target=stream_loop)
+  t2.daemon = True
+  t2.start()
+
+    
+  print("Bot started with Webhook...")
+    
+    
+
+ # ====== Main ======
+if __name__ == "__main__":   
+  # إبقاء البرنامج قيد التشغيل
+  try:
+      while True:
+          time.sleep(1)
+  except KeyboardInterrupt:
+      print("🛑 إيقاف البوت...")
+      bot.remove_webhook()
+
