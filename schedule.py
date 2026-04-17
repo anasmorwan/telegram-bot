af98746a9f106b8e58ebd4c6176b03eaa2b3f636
anasmorwan <anasmorwan5@gmail.com>
2026-04-17 15:05:14 +0200

    Create schedule.py

---
+++ schedule.py
@@ -0,0 +1,32 @@
+from apscheduler.schedulers.background import BackgroundScheduler
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+
+# صباح الجمعة
+scheduler.add_job(
+    send_smart_message,
+    trigger='cron',
+    day_of_week='fri',
+    hour=9,
+    minute=0
+)
+
+# بعد العصر
+scheduler.add_job(
+    send_smart_message,
+    trigger='cron',
+    day_of_week='fri',
+    hour=16,
+    minute=30
+)
+
+# مساء
+scheduler.add_job(
+    send_smart_message,
+    trigger='cron',
+    day_of_week='fri',
+    hour=20,
+    minute=0
+)
